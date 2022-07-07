24cdfebff51964affb1cc8db492e8e5bf1945a46
Alyssandro Dyogo <100590540+Alyssandro1771415@users.noreply.github.com>
2022-07-07 20:58:41 -0300

    Uppando uma copia da pasta manager em razão da pasta original estar impossibilitada.

---
+++ course-manager - Copia/src/app/courses/course.ts
@@ -0,0 +1,12 @@
+export class Course{
+    id: number | any;
+    name!: string;
+    imageURL:string|any;
+    price!:number;
+    code!:string;
+    duration!:number;
+    rating!:number;
+    releaseDate!:string;
+    description: string|any;
+}
+
